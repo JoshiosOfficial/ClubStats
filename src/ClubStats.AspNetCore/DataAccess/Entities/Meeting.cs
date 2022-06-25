09c7cde45ce6c4376c4bd9fc1479656e941d2c7d
JoshiosOfficial <joshuafranco1313@gmail.com>
2022-06-25 01:41:21 -0400

    Add OrganizationId to Meeting to prevent SQL join

--- src/ClubStats.AspNetCore/DataAccess/Entities/Meeting.cs
+++ src/ClubStats.AspNetCore/DataAccess/Entities/Meeting.cs
@@ -4,6 +4,7 @@ public class Meeting
 {
     public Guid Id { get; set; }
     public Organization Organization { get; set; }
+    public Guid OrganizationId { get; set; }
     public string Description { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
